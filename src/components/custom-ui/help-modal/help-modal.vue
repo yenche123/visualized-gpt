3787d431752e82a4965c7b500c4f95c4824f4c5c
yenche123 <tsuiyenche@outlook.com>
2023-03-08 23:01:38 +0800

    deving: help-modal

--- src/components/custom-ui/help-modal/help-modal.vue
+++ src/components/custom-ui/help-modal/help-modal.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
 import { initHelpModal } from './index';
 
 const {
@@ -7,6 +8,8 @@ const {
   duration,
   onTapMask,
 } = initHelpModal()
+
+const { t } = useI18n()
 
 
 </script>
@@ -18,6 +21,33 @@ const {
 
     <div class="hm-box">
 
+      <!-- OpenAI Key -->
+      <div class="hm-part">
+        <div class="hm-hd">
+          <span>OpenAI Key</span>
+        </div>
+        <div class="hm-desc">
+          <span>{{ t('help.openai_key_desc') }}</span>
+        </div>
+
+        <input class="hm-input" :placeholder="t('help.openai_key_ph')" />
+      </div>
+
+      <!-- Open Source -->
+      <div class="hm-part">
+        <div class="hm-hd">
+          <span>{{ t('help.open_source') }}</span>
+        </div>
+        <a class="hm-btn" 
+          href="https://github.com/yenche123/visualized-gpt"
+          target="_blank"
+        >
+          <svg-icon name="github" :coverFillStroke="false" class="hm-opensource-icon"></svg-icon>
+          <div class="hm-btn-text">
+            <span>{{ t('help.check_it_out') }}</span>
+          </div>
+        </a>
+      </div>
 
     </div>
   </div>
